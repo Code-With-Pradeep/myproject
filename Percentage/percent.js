dc47b18761132e4743ffa2c5f6c9d5493c68f4bd
MasterCoder <123901380+MasterCoder1805@users.noreply.github.com>
2023-07-19 22:18:25 +0530

    Add files via upload

---
+++ Percentage/percent.js
@@ -0,0 +1,48 @@
+$('.html').percentcircle({
+
+    animate : true,
+    diameter : 100,
+    guage: 2,
+    coverBg: '#fff',
+    bgColor: '#efefef',
+    fillColor: '#111',
+    percentSize: '20px',
+    percentWeight: 'normal'
+    
+    });
+$('.css').percentcircle({
+
+animate : true,
+diameter : 100,
+guage: 2,
+coverBg: '#fff',
+bgColor: '#efefef',
+fillColor: '#5c93c8',
+percentSize: '15px',
+percentWeight: 'normal'
+
+});
+$('.Javascript').percentcircle({
+
+animate : true,
+diameter : 100,
+guage: 2,
+coverBg: '#fff',
+bgColor: '#efefef',
+fillColor: '#5c93c8',
+percentSize: '15px',
+percentWeight: 'normal'
+
+});
+$('.JQuery').percentcircle({
+
+animate : true,
+diameter : 100,
+guage: 2,
+coverBg: '#fff',
+bgColor: '#efefef',
+fillColor: '#5c93c8',
+percentSize: '15px',
+percentWeight: 'normal'
+
+});
